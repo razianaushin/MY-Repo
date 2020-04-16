959834fd6662dce1fa4595f7d4c3734e84aaebfd
razia123 <razia.naushin124@gmail.com>
2020-04-16 23:19:53 +0600

    Impliment  Amazon Report Test Script

---
+++ Amazon_Keywords_Density_Report.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import sys
+
+
+
+url1 = "https://report-pages.wugo.io/v1/oauth/token"
+
+header1 = {
+ "grant_type":"password",
+ "username": "testuser@gmail.com",
+ "password": "12345678",
+ "client_id": 2,
+ "client_secret": "CM9hRWfZLVeBQ2Gi2ZgK5XUUbfp0AViM8LUVTxVq"
+
+}
+
+response1 = requests.post(url1, data=header1)
+message = response1.json()	
+
+# message['access_token'] is access token you need to use for other API's
+value = str(message['token_type']) + " " + str(message['access_token'])
+
+
+url = "https://report-pages.wugo.io/report/keepa/keyword-density"
+
+
+payload = "  {\"data\": { \"asins\": [1371] }, \"request_type\": \"get_json\" }"
+headers = {
+  'Authorization': value,
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data = payload)
+print(response.json())
+
+
